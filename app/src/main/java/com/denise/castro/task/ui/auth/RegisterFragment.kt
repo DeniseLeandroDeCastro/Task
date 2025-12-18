0d5b39a5f6d13ca8e5d7b6c4b07534a731d818b5
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2025-12-18 20:49:05 -0300

    Adiciona novas dependências ao gradle, ajusta layouts e adiciona o HomeFragment

--- app/src/main/java/com/denise/castro/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/denise/castro/task/ui/auth/RegisterFragment.kt
@@ -5,14 +5,71 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.denise.castro.task.R
+import com.denise.castro.task.databinding.FragmentRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.buttonRegister.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        val email = binding.editTextEmailRegister.text.toString().trim()
+        val password = binding.editTextSenhaRegister.text.toString().trim()
+
+        if(email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+                binding.progressBarRegister.isVisible = true
+                registerUser(email, password)
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                } else {
+                    binding.progressBarRegister.isVisible = false
+                }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
