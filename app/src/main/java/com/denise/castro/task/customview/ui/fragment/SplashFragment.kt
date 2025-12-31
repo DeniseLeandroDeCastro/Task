2fad3ef555d2f07efa98c893b56f522fd3605fbe
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2025-12-31 08:31:30 -0300

    Ajustes nas telas

--- app/src/main/java/com/denise/castro/task/customview/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/denise/castro/task/customview/ui/fragment/SplashFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.denise.castro.task.R
+import com.denise.castro.task.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
     private lateinit var binding: FragmentSplashBinding
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
